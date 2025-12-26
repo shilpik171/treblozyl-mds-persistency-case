9883fe73beabf2b4accac051e0e0c6e7bad280d6
shilpik171 <shilpik171@gmail.com>
2025-12-26 22:31:18 +0530

    Update 01_daily_dose.sql

--- sql/01_daily_dose.sql
+++ sql/01_daily_dose.sql
@@ -1,1 +1,8 @@
-
+ WITH daily_dose AS (
+    SELECT
+        patient_id,
+        service_date,
+        SUM(vials) AS total_vials
+     FROM  `peerless-summit-311611.peerless.patient`
+    GROUP BY patient_id, service_date
+)
