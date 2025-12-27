f5157b11dd50c62c425e54627d191392bc83bf11
shilpik171 <shilpik171@gmail.com>
2025-12-27 18:48:39 +0530

    Update 08_Persistency_metrics_omparison.sql

--- sql/08_Persistency_metrics_omparison.sql
+++ sql/08_Persistency_metrics_omparison.sql
@@ -91,7 +91,6 @@ SELECT
     any_modification,
     COUNT(DISTINCT patient_id) AS patients,
     AVG(persistency_days) AS avg_persistency_days,
-    PERCENTILE_CONT(0.5) 
-        WITHIN GROUP (ORDER BY persistency_days) AS median_persistency_days
+    APPROX_QUANTILES(persistency_days, 2)[OFFSET(1)] AS median_persistency_days
 FROM patient_persistency
 GROUP BY any_modification;
